2987bffd13509c9b75762aa6121eef44b1a755af
Nathan Co <Nathanco1015@tamu.edu>
2024-10-09 17:55:51 -0500

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -1,10 +1,10 @@
-SELECT Name FROM Students;
-SELECT * FROM Students WHERE Age > 30;
-SELECT Name FROM Students WHERE Gender='F' AND Age=30;
-SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
-UPDATE Students SET Points = 500 WHERE Name='Basma';
-UPDATE Students SET Points = 100 WHERE Name='Alex';
+SELECT Name FROM students; # Get all the names of students in the database
+SELECT * FROM students WHERE Age > 30; #Get all the data of students above 30 years old
+SELECT Name FROM students WHERE Gender='F' AND Age=30; #Get the names of the females who are 30 years old
+SELECT Points FROM students WHERE Name='Alex'; #Get the number of Points of Alex
+INSERT INTO students (ID, Name, Age, Gender, Points) VALUES (7, 'Nathan', 21, 'M', 500); #Add yourself as a new student (your name, your age...)
+UPDATE students SET Points = 500 WHERE Name='Basma'; #Increase the points of Basma because she solved a new exercise
+UPDATE students SET Points = 100 WHERE Name='Alex'; #Decrease the points of Alex because he's late today
 
 CREATE TABLE graduates(
     ID INTEGER  PRIMARY KEY AUTOINCREMENT,
